bf1694dd517c4973d72ab1a03fb34203269cc848
nniicks <jhonathanduartebarreto@gmail.com>
2024-12-05 15:09:17 -0400

    tentativa de solucao do problema da mochila com bonuns usando Pyomo

--- mochila_bonus.py
+++ mochila_bonus.py
@@ -1,4 +1,6 @@
 import pyomo.environ as pyEnv
+import pulp
+
 
 caminho = r"C:\Users\jhona\anaconda3\Library\bin\glpsol.exe"
 
@@ -10,11 +12,11 @@ def mochila_bonus(nome_arquivo):
         # lendo o nome da intancia
         referencia = file.readline().strip()
 
-        # lendo a quantidade de variaveis
+        # lendo a quantidade de itens
         n = int(file.readline().strip())
 
         # lendo os valores do n itens
-        valores_itens = list(map(int, file.readline().strip().split()))
+        valor_i = list(map(int, file.readline().strip().split()))
 
          # inicializando a matriz de bonus com 0
         bonus_ij = [[0] * n for _ in range(n)]
@@ -38,41 +40,64 @@ def mochila_bonus(nome_arquivo):
         # lendo os pesos dos itens
         pesos_itens = list(map(int, file.readline().strip().split()))
 
+        '''print(valor_i)
+        print(capacidade)
+        print(pesos_itens)
+        print(bonus_ij)'''
 
-    # =================== REALIZANDO A MODELAGEM ======================== #
 
-    # criando o modelo
+    ############################ REALIZANDO A MODELAGEM DO PROBLEMA ############################
+
+    # ==== CRIANDO O MODELO
     modelo = pyEnv.ConcreteModel()
 
-    # definindo o conjunto de itens
     modelo.I = range(n)
+    modelo.J = range(n)
+    modelo.ab = [(i,j) for i in modelo.I for j in modelo.I if i < j]
 
-    # definindo as variaveis de decisao
+    # criando as variavei de decisao do problema
     modelo.x = pyEnv.Var(modelo.I, within=pyEnv.Binary)
+    modelo.y = pyEnv.Var(modelo.ab, within=pyEnv.Binary)
 
-    # funcao objetivo: maximizar a soma dos valores lieres + valores quadraticos
-    modelo.objetivo = pyEnv.Objective(expr=sum(valores_itens[i] * modelo.x[i] for i in modelo.I),
-                                    sense=pyEnv.maximize)
-    
+    # passando os valores lidos para o modelo
+    modelo.p = pyEnv.Param(modelo.I, initialize=lambda modelo, i: pesos_itens[i]) # peso de cada item
+    modelo.v = pyEnv.Param(modelo.I, initialize=lambda modelo, i: valor_i[i])  # valor de cada item
+    modelo.b = pyEnv.Param(modelo.I, modelo.J, initialize=lambda modelo, i, j: bonus_ij[i][j])  # bonus do item i com o item j
 
-    # restricoes
-    modelo.rest = pyEnv.Constraint(expr=sum(pesos_itens[i] * modelo.x[i] for i in modelo.I) <= capacidade)
+    # ==== FUNCAO OBJETIVO
 
-    # ------------------ RESOLUCAO DO MODELO ----------------------- #
+    # primeiro somatorio (pegando apenas o valor do item i que esta na solucao)
+    soma_x = sum(modelo.v[i] * modelo.x[i] for i in modelo.I)
+
+    # segundo somatorio (pegando o bonus do item i com o item j casos ambos estejam na solucao)
+    soma_y = sum(modelo.b[i,j] * modelo.y[i,j] for i,j in modelo.ab)
+
+    # adicionando a funcao objetivo ao modelo
+    modelo.objetivo = pyEnv.Objective(rule=(soma_x+soma_y), sense=pyEnv.maximize)
+
+    # ==== RESTRICOES
+
+    # restricao 1: os itens colocados na mochila devem ter um peso total <= capacidade da mochila
+    modelo.rest1 = pyEnv.Constraint(modelo.J, sum(modelo.p[i] * modelo.x[i] for i in modelo.I) <= capacidade)
+
+    # restricao 2: sistemas de equacoes que garante que o bonus so seja colocado na solucao quando ambos os itens estivem na solucao
+    modelo.rest2 = pyEnv.Constraint(modelo.ab, modelo.y[i,j] <= modelo.x[i])
+    modelo.rest3 = pyEnv.Constraint(modelo.ab, modelo.y[i,j] <= modelo.x[j])
+    modelo.rest4 = pyEnv.Constraint(modelo.ab, modelo.y[i,j] >= modelo.x[i] + modelo.x[j] - 1)
+
+    # ==== SOLVER
     solver = pyEnv.SolverFactory('glpk')
+    resultado = solver.solve(modelo, tee=True)
 
-    # mostrando na tela o log do server
-    solver = solver.solve(modelo, tee=True)
+    # obtendo o valor da solucao
+    resultado_funcao = pyEnv.value(modelo.objetivo)
 
+    mochila = []
 
-    # ------------------- IMPRIMINDO AS VARIAVEIS DE DECISAO --------------------
-    lista = list(modelo.x.keys())
-    for i in lista:
-        print(i, '--', modelo.x[i]())
-    
+    # guardando na mochila os itens colocados na solucao
+    for i in modelo.I:
+        mochila.append(modelo.x[i]())
 
-    print('Valor objetivo = ', modelo.objetivo())
+    return resultado_funcao, mochila
 
-    
-
-mochila_bonus("teste.txt")
+resultado_funcao, mochila = mochila_bonus("teste.txt")
